f266b7bfa7eb9dae8a685d53006fe2fc67b22ee6
Balabanov, Oleksandr <oleksandr.balabanov@mckesson.com>
2023-04-23 23:51:23 +0200

    [Task-10] - Backend For Frontend:    - added cache 2 minutes for "getProductsList";

--- bff-service/routes/product.routes.js
+++ bff-service/routes/product.routes.js
@@ -1,9 +1,12 @@
 import Router from 'express';
+import apicache from 'apicache';
+
 import ProductsController from '../controllers/ProductController.js';
 
 const productRoutes = new Router();
+const cache = apicache.middleware;
 
-productRoutes.get('/products', ProductsController.getAll);
+productRoutes.get('/products', cache('2 minutes'), ProductsController.getAll);
 productRoutes.get('/product', ProductsController.getOne);
 productRoutes.post('/products', ProductsController.create);
 
